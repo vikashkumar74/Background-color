fee1ef3eb32f2d166499b4c47fdf276f6e3fc1d4
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2023-08-13 01:48:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+document.querySelector('#orange').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'orange';
+});
+document.querySelector('#violet').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'violet';
+});
+
+document.querySelector('#blue').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'blue';
+});
+
+document.querySelector('#green').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'green';
+});
+document.querySelector('#yellow').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'yellow';
+})
+document.querySelector('#pink').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'pink';
+})
+document.querySelector('#red').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'red';
+})
+document.querySelector('#purple').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'purple';
+})
+document.querySelector('#gray').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'gray';
+})
